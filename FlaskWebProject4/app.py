04be333ae106eceb8e7c2dbc2c5aba4e98745c50
Farianskiy <mr.avgustins@mail.ru>
2023-03-20 10:55:14 +0300

    Добавьте файлы проекта.

---
+++ FlaskWebProject4/app.py
@@ -0,0 +1,31 @@
+
+
+from flask import Flask
+
+app = Flask(__name__)
+app.debug = True
+
+@app.route('/')
+def index():
+    return 'Home Page'
+
+@app.route('/career/')
+def career():
+    return 'Career Page'
+
+@app.route('/feedback/')
+def feedback():
+    return 'Feedback Page'
+
+@app.route('/user/<int:id>/')
+def user_profile(id):
+    return "Profile page of user #{}".format(id)
+
+if __name__ == '__main__':
+    import os
+    HOST = os.environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT)
---
+++ FlaskWebProject4/main2.py
@@ -0,0 +1,25 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello Flask'
+
+@app.route('/user/<int:user_id>/')
+def user_profile(user_id):
+    return "Profile page of user #{}".format(user_id)
+
+@app.route('/books/<string:genre>/')
+def books(genre):
+    return "All Books in {} category".format(genre)
+
+
+if __name__ == '__main__':
+    import os
+    HOST = os.environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT)
