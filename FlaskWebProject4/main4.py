c54cf6fc0f52e852988a61e7d2fdd5c769edc303
Farianskiy <mr.avgustins@mail.ru>
2023-03-21 11:02:04 +0300

    Урок 4

---
+++ FlaskWebProject4/main4.py
@@ -0,0 +1,22 @@
+
+
+from main2 import app
+from flask import request, current_app
+
+
+with app.test_request_context('/products'):
+    request.path  # получим полный путь к запрашиваемой странице(без домена).
+    request.method
+    current_app.name
+
+
+    
+
+if __name__ == '__main__':
+    import os
+    HOST = os.environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT)
